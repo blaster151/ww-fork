ed5a4efee971f93e707b7f7a8e9fb7cbdc4e07f3
Jeff Benson <blaster151@gmail.com>
2017-07-15 15:27:09 -0500

    Add angular2-modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
-
 import { AppComponent } from './app.component';
 import { XmlToJsonService } from './xml-to-json.service';
 import { environment } from '../environments/environment';
+import { BootstrapModalModule } from 'angular2-modal/plugins/bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,8 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    BootstrapModalModule
   ],
   providers: [
     XmlToJsonService
