fbb8c13b92bef4cfcf914235a84ecfa7619526ef
Jeff Benson <blaster151@gmail.com>
2017-10-21 18:31:03 -0500

    Added ratio setting to dynamic font size directive

--- src/app/dynamic-font-size.directive.ts
+++ src/app/dynamic-font-size.directive.ts
@@ -1,16 +1,19 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appDynamicFontSize]'
 })
 export class DynamicFontSizeDirective {
+  @Input('appDynamicFontSize') ratio: number;
 
   constructor(private elementRef: ElementRef) {
-    console.log('in ctr of dfsd');
-   }
+  }
 
   ngOnInit() {
     console.log('in dynamic FS directive', this.elementRef.nativeElement.offsetHeight);
+    console.log('in ngOnInit of dfsd', this.ratio);
+    if (!this.ratio)
+      this.ratio = .1;
 
     setTimeout(() => {
       this.resizeFont();
@@ -20,11 +23,11 @@ export class DynamicFontSizeDirective {
   }
 
   resizeFont() {
-    let fontSize = Math.round(this.elementRef.nativeElement.offsetHeight / 22);
-    console.log(fontSize);
+    let fontSize = Math.round(document.querySelector("body").offsetHeight * this.ratio);//this.elementRef.nativeElement.offsetHeight
+    console.log('font size ', fontSize);
     this.elementRef.nativeElement.style.fontSize = fontSize + "px";
-    this.elementRef.nativeElement.style.lineHeight = fontSize + "px";
-
+    //this.elementRef.nativeElement.style.lineHeight = fontSize + "px";
+    console.log('body ', document.querySelector("body").offsetHeight);
   }
 
 }
