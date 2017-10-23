790e008d2edbb037b002be982d14d858bed38fa3
Jeff Benson <blaster151@gmail.com>
2017-10-23 15:26:38 -0500

    Discern landscape vs portrait modes; apply different font ratios

--- src/app/dynamic-font-size.directive.ts
+++ src/app/dynamic-font-size.directive.ts
@@ -6,11 +6,17 @@ import { Directive, ElementRef, Input } from '@angular/core';
 export class DynamicFontSizeDirective {
   @Input('appDynamicFontSize') ratio: number;
   @Input('setbody') setbody: boolean;
+  private portraitMode = false;
 
   constructor(private elementRef: ElementRef) {
   }
 
   ngOnInit() {
+    console.log('orientation');
+    console.log(window);
+
+    this.detectPortraitMode();
+
     console.log('in dynamic FS directive', this.elementRef);
     console.log('in dynamic FS directive', this.elementRef.nativeElement);
     console.log('in dynamic FS directive', this.elementRef.nativeElement.offsetHeight);
@@ -30,14 +36,33 @@ export class DynamicFontSizeDirective {
       console.log('discerned setBody');
 
     } 
-    window.addEventListener('resize', () => { setTimeout(() => { this.resizeFont() }, 50) }, false);
+    window.addEventListener('resize', () => { setTimeout(() => { this.detectPortraitMode(); this.resizeFont() }, 50) }, false);
   }
 
+  private detectPortraitMode() {
+    if (window.innerHeight > window.innerWidth){
+      this.portraitMode = true;
+    }
+  }
+
+  private getInitialBodyHeight() {
+    let bodyHeight = document.querySelector("body").offsetHeight;
+
+    console.log('bodyheight ', bodyHeight);
+    return bodyHeight;
+  }
   resizeFont() {
     // Discern somehow whether we're in portrait mode
-    let ratioToUse = this.ratio * 0.6;
+    let ratioToUse = this.ratio * 1.1;  // Default to landscape
 
-    let fontSize = Math.round(document.querySelector("body").offsetHeight * ratioToUse);//this.elementRef.nativeElement.offsetHeight
+    // Adjust for portrait mode
+    if (this.portraitMode)
+    {
+      console.log('adjusting for portrait mode');
+      ratioToUse *= 1.0;
+    }
+
+    let fontSize = Math.round(this.getInitialBodyHeight() * ratioToUse);//this.elementRef.nativeElement.offsetHeight
     console.log('font size ', fontSize);
     this.elementRef.nativeElement.style.fontSize = fontSize + "px";
     //this.elementRef.nativeElement.style.lineHeight = fontSize + "px";
