388229b68e72e321ac71fe732e7403e8d5c99e8f
Jeff Benson <jeff.benson@docusign.com>
2018-05-18 13:44:53 -0500

    Updates

--- pushzip.bat
+++ pushzip.bat
@@ -1,12 +1,6 @@
 call ng build --prod
-call xcopy dist\*.* ..\dists\wonderword /e /y
+call xcopy dist\*.* ..\dists\wonderwordzip /e /y
 call cd ..\dists
 call del wonderword.zip
-call powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('wonderword', 'wonderword.zip'); }"
+call powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('wonderwordzip', 'wonderword.zip'); }"
 copy wonderword.zip "C:\Users\Jeff Benson\Dropbox\Public\Wonderword"
-
-REM call git stage *
-REM call git commit -m Rebuilt
-REM call git push
-REM call cd ..\..\ww-fork
-REM call start https://appharbor.com/applications/wonderword
